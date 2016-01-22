e82138f5adbe5d4f2e44a792d9b29d8ce2ad5f13
Alex Kuefler <kuefleralex@gmail.com>
2016-01-21 18:22:35 -0800

    Added logic for downsampling images, or importing as grayscale.

--- loadData.py
+++ loadData.py
@@ -2,12 +2,13 @@ import os
 import numpy as np
 from collections import Counter
 
+import matplotlib as mpl
+
 import scipy
 import scipy.io
 import scipy.ndimage
 
-
-x = 4
+##ISSUE: should do more pre-processing. Subtract mean images. etc.
 
 class Trial():
     def __init__(self, cat, idx, img, trajs):
@@ -21,26 +22,35 @@ def cropImage(img):
     xs, ys = np.where(np.invert(B) == True)
     return img[xs[0]:xs[-1], ys[0]:ys[-1]]
 
-def sampleCAT(n = 1000):
+def sampleCAT(n = 1000, size= 1, asgray= False, categories= None):
+    """
+    WARNING: resizing the image will mess up the fixation points.
+    """
     sample= []
 
-    categories = os.listdir("./CATdata/Stimuli")[1:]
+    if categories == None:
+        categories = os.listdir("./CATdata/Stimuli")[1:]
+
     counts= Counter(np.random.choice(categories, n, replace= True))
     for cat, count in counts.items():
         stimDir = os.listdir("./CATdata/Stimuli/"+cat)[1:-1]
         trajDir = os.listdir("./CATdata/FIXATIONTRAJS/"+cat)[1:]
 
         catSize = len(stimDir)
-        picks= np.random.choice(np.arange(catSize), count, replace= False)
+        picks= np.random.choice(np.arange(catSize-1), count, replace= False)
 
         for pick in picks:
             IMG = scipy.ndimage.imread('./CATdata/Stimuli/'+cat+'/'+stimDir[pick])
 
-            #Mild pre-processing
+            #Pre-processing
             if len(IMG.shape) == 2:
-                IMG = np.expand_dims(IMG, 2)
-
+                IMG = np.asarray(np.dstack((IMG, IMG, IMG)))
             IMG = cropImage(IMG)
+            if asgray:
+                IMG = mpl.colors.rgb_to_hsv(IMG)[:,:,2]
+
+            IMG = scipy.misc.imresize(IMG, size)
+
             M = scipy.io.loadmat('./CATdata/FIXATIONTRAJS/'+cat+'/'+trajDir[pick])['cellVal'][0][0]
             trajs = {k[0][0][0][0][0]:k[0][0][0][1] for k in M}
 
