f7b9a7f2ee278ec35bccccdcc24a0563901b6d44
Alex Kuefler <kuefleralex@gmail.com>
2016-01-21 18:20:52 -0800

    Perform PCA on separate sample of image glimpses. Store PCA object in Agent as feature extractor.

--- reinforcement.py
+++ reinforcement.py
@@ -1,8 +1,45 @@
 from loadData import sampleCAT
 from matplotlib import pyplot as plt
 import numpy as np
+import sklearn.decomposition as deco
 
 import math
+
+RADIUS = 15
+
+def getPCA(sample, numWins, k):
+    num_components = (2*RADIUS)**2
+    num_samples = len(sample)*numWins
+    assert num_samples > num_components
+
+    X = np.zeros((num_samples, num_components))
+
+    #Create observation matrix:
+    for j, trial in enumerate(sample):
+        img = trial.image
+        try:
+            size_x, size_y, size_z = img.shape
+        except:
+            size_x, size_y = img.shape
+
+        ys = np.random.randint(RADIUS, size_x-RADIUS, numWins)
+        xs = np.random.randint(RADIUS, size_y-RADIUS, numWins)
+
+        for i in range(numWins):
+            try:
+                cm = None
+                window = img[ys[i]-RADIUS:ys[i]+RADIUS, xs[i]-RADIUS:xs[i]+RADIUS, :]
+            except:
+                cm = 'Greys'
+                window = img[ys[i]-RADIUS:ys[i]+RADIUS, xs[i]-RADIUS:xs[i]+RADIUS]
+
+            X[i + numWins*j, :] = window.flatten()
+
+    #Perform PCA
+    pca = deco.RandomizedPCA(n_components= k)
+    pca.fit(X)
+
+    return pca
 
 class Environment():
     def __init__(self, sample, pos):
@@ -15,21 +52,20 @@ class Environment():
 
         #Parameters:
         self.step_size = 60
-        self.radius = 80
         #plt.imshow(self.currImg, interpolation='nearest')
 
     def getLegalActions(self):
         legalActions = ['up', 'down', 'left', 'right']
-        if self.currPos[0] + (self.radius+self.step_size) > self.currImg.shape[0]:
+        if self.currPos[0] + (RADIUS+self.step_size) > self.currImg.shape[0]:
             legalActions.remove('up')
 
-        elif self.currPos[0] - (self.radius+self.step_size) < 0:
+        elif self.currPos[0] - (RADIUS+self.step_size) < 0:
             legalActions.remove('down')
 
-        elif self.currPos[1] + (self.radius+self.step_size) > self.currImg.shape[1]:
+        elif self.currPos[1] + (RADIUS+self.step_size) > self.currImg.shape[1]:
             legalActions.remove('right')
 
-        elif self.currPos[1] - (self.radius+self.step_size) < 0:
+        elif self.currPos[1] - (RADIUS+self.step_size) < 0:
             legalActions.remove('left')
 
         return legalActions
@@ -49,22 +85,19 @@ class Environment():
 
         #self.currWin = self.observe(self.currPos[0], self.currPos[1], self.radius)
 
-
-    def observe(self, x= None, y= None, rad= None):
+    def observe(self, x= None, y= None):
         if x == None:
             x = self.currPos[0]
         if y == None:
             y = self.currPos[1]
-        if rad == None:
-            rad = self.radius
 
         try: #Got a R/G/B image
             #window = self.currImg[x-rad:x+rad, y-rad:y+rad, :]
-            window = self.currImg[y-rad:y+rad, x-rad:x+rad, :]
+            window = self.currImg[y-RADIUS:y+RADIUS, x-RADIUS:x+RADIUS, :]
             cm = None
         except IndexError: #Got a B/W image
             #window = self.currImg[x-rad:x+rad, y-rad:y+rad]
-            window = self.currImg[y-rad:y+rad, x-rad:x+rad]
+            window = self.currImg[y-RADIUS:y+RADIUS, x-RADIUS:x+RADIUS]
             cm = 'Greys'
 
         plt.imshow(window, cmap= cm, interpolation='nearest')
@@ -73,23 +106,32 @@ class Environment():
 
 
 class Agent():
-    def __init__(self, environment):
+    def __init__(self, environment, phi):
         self.env = environment
-
-    def simulate(self):
-        #for i in range(10):
-        while True:
-            window = self.env.observe()
-            a = self.act(window)
-            self.env.update(a)
+        self.phi = phi
 
     def act(self, state, epsilon = 0.5):
         legalActs = self.env.getLegalActions()
         act = np.random.choice(legalActs)
         return act
 
+    def extractFeatures(self, window):
+        win = window.flatten().reshape(1,-1)
+        state = self.phi.transform(win)
+        return state
 
-S = sampleCAT(10, categories= ['Action', 'Indoor', 'Object', 'Affective'])
+    def simulate(self):
+        #for i in range(10):
+        while True:
+            window = self.env.observe()
+            state = self.extractFeatures(window)
+            a = self.act(state)
+            self.env.update(a)
+
+
+S = sampleCAT(130, size= 0.3, asgray= True, categories= ['Action', 'Indoor', 'Object', 'Affective'])
+S1 = S[:10]
+S2 = S[10:]
 try:
     x, y, z = S[0].image.shape
 except:
@@ -98,9 +140,10 @@ except:
 x = math.floor(y/2.0)
 y = math.floor(x/2.0)
 
-env = Environment(S, [x, y])
+pca = getPCA(S2, 10, 100)
+env = Environment(S1, [x, y])
 
-age = Agent(env)
+age = Agent(env, pca)
 age.simulate()
 
 #env.observe(x, y, 30)
