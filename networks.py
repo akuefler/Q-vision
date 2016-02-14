080c50c8efb4d8d6698aa7805c5bec6a85be85ad
Alex Kuefler <kuefleralex@gmail.com>
2016-02-13 21:49:24 -0800

    Added convolutional neural network subclass.

--- networks.py
+++ networks.py
@@ -10,17 +10,34 @@ from collections import OrderedDict
 
 def iterate_minibatches(inputs, targets, batchsize):
     mini_batches = []
+    Dx = inputs.ndim
+    Dy = targets.ndim
     if batchsize == targets.shape[0]:
         mini_batches.append((inputs, targets))
         return mini_batches
 
     for i, x in enumerate(inputs):
-        input_batch = inputs[i*batchsize:i*batchsize + batchsize, :]
-        target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        if Dx == 3:
+            input_batch = inputs[i*batchsize:i*batchsize + batchsize, :]
+            #target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        elif Dx == 4:
+            input_batch = inputs[i*batchsize:i*batchsize + batchsize, :, :]
+            #target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+
+        else:
+            assert False
+
+        if Dy == 2:
+            target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        elif Dy == 1:
+            target_batch = targets[i*batchsize:i*batchsize + batchsize]
+        else:
+            assert False
 
         if input_batch.size == 0 or target_batch.size == 0:
             break
 
+        #if input_batch.shape[0] == batchsize: #ISSUE: Just drop anything the wrong size.
         mini_batches.append((input_batch, target_batch))
 
     return mini_batches
@@ -31,23 +48,34 @@ class NeuralNet(object):
         self.y = T.matrix('targets')
         self.network = None
 
-    def initiliaze(self):
+    def initiliaze(self, mode= 'regress'):
         """
         Create the rest of the attributes/methods after a computational graph has been created.
         """
         #Training parameters:
-        loss = lasagne.objectives.squared_error(lasagne.layers.get_output(self.network), self.y)
-        loss = loss.mean()
+        if mode == 'regress':
+            loss = lasagne.objectives.squared_error(lasagne.layers.get_output(self.network), self.y)
+            loss = loss.mean()
+
+            test_prediction = lasagne.layers.get_output(self.network, deterministic= True)
+            test_loss = lasagne.objectives.squared_error(test_prediction, self.y)
+            test_loss = test_loss.mean()
+        elif mode == 'classify':
+            #loss = lasagne.objectives.categorical_crossentropy(lasagne.layers.get_output(self.network), self.y)
+            loss = lasagne.objectives.multiclass_hinge_loss(lasagne.layers.get_output(self.network), self.y)
+            loss = loss.mean()
+
+            test_prediction = lasagne.layers.get_output(self.network, deterministic= True)
+            test_loss = lasagne.objectives.multiclass_hinge_loss(test_prediction, self.y)
+            test_loss = test_loss.mean()
 
         params = lasagne.layers.get_all_params(self.network, trainable=True)
 
         #updates = lasagne.updates.nesterov_momentum(
         #    loss, params, learning_rate= 1e-7, momentum= 0.9)
-        updates = lasagne.updates.sgd(loss, params, learning_rate= 1e-5)
+        #updates = lasagne.updates.sgd(loss, params, learning_rate= 1e-5)
+        updates = lasagne.updates.adam(loss, params, learning_rate= 1e-5)
 
-        test_prediction = lasagne.layers.get_output(self.network, deterministic= True)
-        test_loss = lasagne.objectives.squared_error(test_prediction, self.y)
-        test_loss = test_loss.mean()
         #test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), self.y),
         #                  dtype=theano.config.floatX)
 
@@ -83,7 +111,7 @@ class NeuralNet(object):
                 losses.append(train_loss)
                 train_batches += 1
 
-            #plt.plot(losses)
+            plt.plot(losses)
 
             val_err = 0.0
             val_batches = 0.0
@@ -94,6 +122,7 @@ class NeuralNet(object):
                 #val_acc += acc
                 val_batches += 1
 
+            print("EPOCH: {:,.1f}".format(epoch))
             print("  training loss:\t\t{:.6f}".format(train_loss / train_batches))
             print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
             #print("  validation accuracy:\t\t{:.2f} %".format(
@@ -131,7 +160,6 @@ class SigNet(NeuralNet):
 
         self.initiliaze()
 
-
 class EuclidNet(NeuralNet):
     def __init__(self, n_features, hidden_size, output_size, batch_size):
         NeuralNet.__init__(self)
@@ -159,3 +187,45 @@ class EuclidNet(NeuralNet):
         self.network = l_out
 
         self.initiliaze()
+
+class ConvBaseline(NeuralNet):
+    def __init__(self, height, width, channels, hidden_size, output_size, batch_size, num_convpools= 2, num_filters= 5):
+        NeuralNet.__init__(self)
+        self.batch_size = batch_size
+
+        self.x = T.tensor4('inputs')
+        self.y = T.lvector('targets')
+        self.layers = []
+
+        l_in = lasagne.layers.InputLayer(shape= (None, channels, height, width),
+                                         input_var= self.x)
+        self.layers.append(l_in)
+
+        prev_layer = l_in
+        for cp in range(num_convpools):
+            conv = lasagne.layers.Conv2DLayer(
+                prev_layer, num_filters= num_filters,
+                filter_size=3, stride= 1, pad=1,
+                nonlinearity=lasagne.nonlinearities.tanh,
+                W=lasagne.init.GlorotUniform())
+            self.layers.append(conv)
+
+            pool = lasagne.layers.MaxPool2DLayer(conv, pool_size=4, stride=4)
+            self.layers.append(pool)
+
+            prev_layer = pool
+
+        l_hid3 = lasagne.layers.DenseLayer(
+            pool, num_units= hidden_size,
+            nonlinearity=lasagne.nonlinearities.tanh, #Rectify may be causing nans in cross entropy loss.
+            W=lasagne.init.GlorotUniform())
+        self.layers.append(l_hid3)
+
+        l_out = lasagne.layers.DenseLayer(
+            l_hid3, num_units= output_size,
+            nonlinearity=lasagne.nonlinearities.linear)
+        self.layers.append(l_out)
+
+        self.network = l_out
+
+        self.initiliaze(mode= 'classify')
