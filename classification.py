0312cc2fd592edea9d199211d806b0b6c47505cb
Alex Kuefler <kuefleralex@gmail.com>
2016-02-14 21:29:13 -0800

    Added combined LSTM/convolutional network architecture.

--- classification.py
+++ classification.py
@@ -21,8 +21,8 @@ from loadData import RADIUS, HORIZON
 def sampleCATdata(n_examples, size, asgray, categories, get_seqs= True, shuffle= True):
     S = sampleCAT(n_examples, size= size, asgray= asgray, categories= categories, get_seqs= get_seqs)
 
-    X_SEQS = None
-    Y_SEQS = []
+    X_SEQ = None
+    Y_SEQ = []
     H, W = S[0].image.shape
     C = 1
 
@@ -39,18 +39,18 @@ def sampleCATdata(n_examples, size, asgray, categories, get_seqs= True, shuffle=
         Y[i] = categories.index(trial.category)
         if get_seqs:
             for subj, seq in trial.sequence.items():
-                if X_SEQS is None:
-                    X_SEQS = np.expand_dims(seq, 0)
+                if X_SEQ is None:
+                    X_SEQ = np.expand_dims(seq, 0)
                 else:
-                    X_SEQS = np.concatenate((X_SEQS, np.expand_dims(seq, 0)), axis= 0)
+                    X_SEQ = np.concatenate((X_SEQ, np.expand_dims(seq, 0)), axis= 0)
 
 
-                Y_SEQS.append(Y[i])
+                Y_SEQ.append(Y[i])
         #for subject, traj in trial.trajs.items():
             #for fixation in traj:
 
     #X_SEQS = np.array(X_SEQS)
-    Y_SEQS = np.array(Y_SEQS)
+    Y_SEQ = np.array(Y_SEQ)
 
     if shuffle:
         p = np.random.permutation(range(N))
@@ -61,25 +61,25 @@ def sampleCATdata(n_examples, size, asgray, categories, get_seqs= True, shuffle=
     Y_train, Y_val, Y_test = np.array_split(Y, [np.floor(0.5 * N), np.floor(0.75*N)])
 
     if get_seqs:
-        N_SEQS = len(X_SEQS)
-        p = np.random.permutation(range(N_SEQS))
-        X_SEQS = X_SEQS[p]
-        Y_SEQS = Y_SEQS[p]
+        N_SEQ = len(X_SEQ)
+        p = np.random.permutation(range(N_SEQ))
+        X_SEQ = X_SEQ[p]
+        Y_SEQ = Y_SEQ[p]
 
-        X_SEQS_train, X_SEQS_val, X_SEQS_test = np.array_split(X_SEQS, [np.floor(0.5 * N_SEQS), np.floor(0.75*N_SEQS)])
-        Y_SEQS_train, Y_SEQS_val, Y_SEQS_test = np.array_split(Y_SEQS, [np.floor(0.5 * N_SEQS), np.floor(0.75*N_SEQS)])
+        X_SEQ_train, X_SEQ_val, X_SEQ_test = np.array_split(X_SEQ, [np.floor(0.5 * N_SEQ), np.floor(0.75*N_SEQ)])
+        Y_SEQ_train, Y_SEQ_val, Y_SEQ_test = np.array_split(Y_SEQ, [np.floor(0.5 * N_SEQ), np.floor(0.75*N_SEQ)])
 
         return X_train, X_val, X_test, Y_train, Y_val, Y_test,\
-               X_SEQS_train, X_SEQS_val, X_SEQS_test, Y_SEQS_train, Y_SEQS_val, Y_SEQS_test
+               X_SEQ_train, X_SEQ_val, X_SEQ_test, Y_SEQ_train, Y_SEQ_val, Y_SEQ_test
 
     return X_train, X_val, X_test, Y_train, Y_val, Y_test
 
 if True:
     categories= ['Action', 'Indoor', 'Object', 'Affective']
     size = (500, 500)
-    N = 20
+    N = 100
     X_train, X_val, X_test, Y_train, Y_val, Y_test,\
-    X_SEQS_train, X_SEQS_val, X_SEQS_test, Y_SEQS_train, Y_SEQS_val, Y_SEQS_test = sampleCATdata(N, size= size, asgray= True, categories= categories)
+    X_SEQ_train, X_SEQ_val, X_SEQ_test, Y_SEQ_train, Y_SEQ_val, Y_SEQ_test = sampleCATdata(N, size= size, asgray= True, categories= categories)
 
     #cnn = ConvBaseline(size[0], size[1], 1, 300, len(categories), batch_size= 10)
     print "Constructing Recurrent CNN. This should take a bit..."
@@ -87,7 +87,7 @@ if True:
     H = 2*RADIUS
     rcnn = ConvLSTM(H, H, 1, HORIZON, 300, len(categories), batch_size= 10)
 
-    rcnn.predict(X_SEQS_train[0:10])
+    #rcnn.predict(X_SEQS_train[0:10])
 
 
 else:
@@ -120,13 +120,23 @@ else:
     Y_train, Y_val, Y_test = np.array_split(Y, 3)
     cnn = ConvBaseline(32, 32, C, 500, 10, batch_size= 200, num_filters= 20)
 
-##Highest CIFAR accuracy so far: ~0.21 w 20 filters, 500 hidden units, 60 epochs
-cnn.train(X_train, Y_train, X_val, Y_val, num_epochs= 60) #Lower learning rate seems to make a considerable diff.
+if False:
+    ##Highest CIFAR accuracy so far: ~0.21 w 20 filters, 500 hidden units, 60 epochs
+    cnn.train(X_train, Y_train, X_val, Y_val, num_epochs= 60) #Lower learning rate seems to make a considerable diff.
 
-error = mets.accuracy_score(Y_train, cnn.predict(X_train).argmax(axis = 1))
-print("Accuracy on training set", error)
+    error = mets.accuracy_score(Y_train, cnn.predict(X_train).argmax(axis = 1))
+    print("Accuracy on training set", error)
 
-error = mets.accuracy_score(Y_test, cnn.predict(X_test).argmax(axis = 1))
-print("Accuracy on test set", error)
+    error = mets.accuracy_score(Y_test, cnn.predict(X_test).argmax(axis = 1))
+    print("Accuracy on test set", error)
+
+if True:
+    rcnn.train(X_SEQ_train, Y_SEQ_train, X_SEQ_val, Y_SEQ_val, num_epochs= 10)
+
+    error = mets.accuracy_score(Y_SEQ_train, rcnn.predict(X_SEQ_train).argmax(axis = 1))
+    print("Accuracy on training set", error)
+
+    error = mets.accuracy_score(Y_SEQ_test, rcnn.predict(X_SEQ_test).argmax(axis = 1))
+    print("Accuracy on test set", error)
 
 halt= True
--- networks.py
+++ networks.py
@@ -17,22 +17,25 @@ def iterate_minibatches(inputs, targets, batchsize):
         return mini_batches
 
     for i, x in enumerate(inputs):
-        if Dx == 3:
-            input_batch = inputs[i*batchsize:i*batchsize + batchsize, :]
-            #target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
-        elif Dx == 4:
-            input_batch = inputs[i*batchsize:i*batchsize + batchsize, :, :]
-            #target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        ##ISSUE: This is all probably totally unnecessary...
+        #if Dx == 3:
+            #input_batch = inputs[i*batchsize:i*batchsize + batchsize, :]
+            ##target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        #elif Dx == 4:
+            #input_batch = inputs[i*batchsize:i*batchsize + batchsize, :, :]
+            ##target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
 
-        else:
-            assert False
+        #elif Dx == 5:
+            #assert False
+        input_batch = inputs[i*batchsize:i*batchsize + batchsize, :, :]
 
-        if Dy == 2:
-            target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
-        elif Dy == 1:
-            target_batch = targets[i*batchsize:i*batchsize + batchsize]
-        else:
-            assert False
+        #if Dy == 2:
+            #target_batch = targets[i*batchsize:i*batchsize + batchsize, :]
+        #elif Dy == 1:
+            #target_batch = targets[i*batchsize:i*batchsize + batchsize]
+        #else:
+            #assert False
+        target_batch = targets[i*batchsize:i*batchsize + batchsize]
 
         if input_batch.size == 0 or target_batch.size == 0:
             break
@@ -68,6 +71,17 @@ class NeuralNet(object):
             test_prediction = lasagne.layers.get_output(self.network, deterministic= True)
             test_loss = lasagne.objectives.multiclass_hinge_loss(test_prediction, self.y)
             test_loss = test_loss.mean()
+        elif mode == 'sequence':
+            ##Only consider last label of the sequence?
+            loss = lasagne.objectives.multiclass_hinge_loss(lasagne.layers.get_output(self.network), self.y)
+            loss = loss.mean()
+
+            test_prediction = lasagne.layers.get_output(self.network, deterministic= True)
+            test_loss = lasagne.objectives.multiclass_hinge_loss(test_prediction, self.y)
+            test_loss = test_loss.mean()
+
+        else:
+            assert False
 
         params = lasagne.layers.get_all_params(self.network, trainable=True)
 
@@ -75,9 +89,6 @@ class NeuralNet(object):
         #    loss, params, learning_rate= 1e-7, momentum= 0.9)
         #updates = lasagne.updates.sgd(loss, params, learning_rate= 1e-5)
         updates = lasagne.updates.adam(loss, params, learning_rate= 1e-5)
-
-        #test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), self.y),
-        #                  dtype=theano.config.floatX)
 
         ##Methods:
         self.weight_update = theano.function(inputs= [self.x, self.y], outputs= loss, updates= updates)
@@ -111,7 +122,7 @@ class NeuralNet(object):
                 losses.append(train_loss)
                 train_batches += 1
 
-            plt.plot(losses)
+            #plt.plot(losses)
 
             val_err = 0.0
             val_batches = 0.0
@@ -231,6 +242,43 @@ class ConvBaseline(NeuralNet):
         self.initiliaze(mode= 'classify')
 
 
+class ConvRNN(NeuralNet):
+    def __init__(self, height, width, channels, timesteps, hidden_size, output_size, batch_size, num_convpools= 2, num_filters= 5):
+        NeuralNet.__init__(self)
+        self.batch_size = batch_size
+
+        tensor5 = T.TensorType('float64', [False]*5)
+        self.x = tensor5('inputs')
+        self.y = T.lvector('targets')
+        self.layers = []
+
+        n_batch, n_steps, n_channels, width, height = (batch_size, timesteps, channels, width, height)
+        n_out_filters = 7
+        filter_shape = (3, 3)
+
+        l_in = lasagne.layers.InputLayer(
+             (None, n_steps, n_channels, width, height),
+             input_var= self.x)
+        l_in_to_hid = lasagne.layers.Conv2DLayer(
+             lasagne.layers.InputLayer((None, n_channels, width, height)),
+             n_out_filters, filter_shape, pad='same')
+        l_hid_to_hid = lasagne.layers.Conv2DLayer(
+             lasagne.layers.InputLayer(l_in_to_hid.output_shape),
+             n_out_filters, filter_shape, pad='same')
+        l_rec = lasagne.layers.CustomRecurrentLayer(
+             l_in, l_in_to_hid, l_hid_to_hid)
+
+        l_reshape = lasagne.layers.ReshapeLayer(l_rec, (-1, np.prod(l_rec.output_shape[2:])))
+
+        l_out = lasagne.layers.DenseLayer(
+            l_reshape, num_units= output_size,
+            nonlinearity=lasagne.nonlinearities.linear)
+
+        self.layers = [l_in, l_rec, l_out]
+        self.network = l_out
+
+        self.initiliaze(mode= 'classify')
+
 class ConvLSTM(NeuralNet):
     def __init__(self, height, width, channels, timesteps, hidden_size, output_size, batch_size, num_convpools= 2, num_filters= 5):
         NeuralNet.__init__(self)
@@ -246,24 +294,313 @@ class ConvLSTM(NeuralNet):
         filter_shape = (3, 3)
 
         l_in = lasagne.layers.InputLayer(
-             (n_batch, n_steps, n_channels, width, height),
+             (None, n_steps, n_channels, width, height),
              input_var= self.x)
-        l_in_to_hid = lasagne.layers.Conv2DLayer(
-             lasagne.layers.InputLayer((None, n_channels, width, height)),
-             n_out_filters, filter_shape, pad='same')
-        l_hid_to_hid = lasagne.layers.Conv2DLayer(
-             lasagne.layers.InputLayer(l_in_to_hid.output_shape),
-             n_out_filters, filter_shape, pad='same')
-        l_rec = lasagne.layers.CustomRecurrentLayer(
-             l_in, l_in_to_hid, l_hid_to_hid)
+        l_reshape1 = lasagne.layers.ReshapeLayer(l_in, (-1, l_in.output_shape[2], l_in.output_shape[3], l_in.output_shape[4]))
+        conv = lasagne.layers.Conv2DLayer(
+            l_reshape1,
+            n_out_filters, filter_shape, pad='same')
+        pool = lasagne.layers.MaxPool2DLayer(conv, pool_size=4, stride=4)
+        linear = lasagne.layers.DenseLayer(pool, num_units= hidden_size, nonlinearity= lasagne.nonlinearities.linear)
+        l_reshape2 = lasagne.layers.ReshapeLayer(linear, (-1, timesteps, hidden_size))
+        lstm = lasagne.layers.LSTMLayer(l_reshape2, hidden_size)
 
-        l_reshape = lasagne.layers.ReshapeLayer(l_rec, (n_batch * n_steps, np.prod(l_rec.output_shape[2:])))
+        dense1 = lasagne.layers.DenseLayer(
+            pool, num_units= hidden_size,
+            nonlinearity=lasagne.nonlinearities.tanh, #Rectify may be causing nans in cross entropy loss.
+            W=lasagne.init.GlorotUniform())
 
-        l_out = lasagne.layers.DenseLayer(
-            l_reshape, num_units= output_size,
+        dense2 = lasagne.layers.DenseLayer(
+            dense1, num_units= output_size,
             nonlinearity=lasagne.nonlinearities.linear)
 
-        self.layers = [l_in, l_rec, l_out]
+        #Compute argmax of all timesteps?
+        l_reshape3 = lasagne.layers.ReshapeLayer(dense2, (-1, output_size, timesteps))
+        l_out = lasagne.layers.GlobalPoolLayer(l_reshape3, pool_function=T.mean) #Using mean instead of max seems to improve val. accuracy.
+        #l_out = dense2
+
+        #self.layers = [l_in, l_rec, l_out]
         self.network = l_out
 
-        self.initiliaze(mode= 'classify')
+        self.initiliaze(mode= 'sequence')
+
+#class ConvLSTM(NeuralNet):
+    #def __init__(self, height, width, channels, timesteps, hidden_size, output_size, batch_size, num_convpools= 2, num_filters= 5):
+        #NeuralNet.__init__(self)
+        #self.batch_size = batch_size
+
+        #tensor5 = T.TensorType('float64', [False]*5)
+        #self.x = tensor5('inputs')
+        #self.y = T.lvector('targets')
+        #self.layers = []
+
+        #n_batch, n_steps, n_channels, width, height = (batch_size, timesteps, channels, width, height)
+        #n_out_filters = 7
+        #filter_shape = (3, 3)
+
+        #l_in = lasagne.layers.InputLayer(
+             #(None, n_steps, n_channels, width, height),
+             #input_var= self.x)
+        #l_in_to_hid = ConvPoolLayer(
+             #lasagne.layers.InputLayer((None, n_channels, width, height)),
+             #n_out_filters, filter_shape, pad='same')
+        ##l_hid_to_hid = lasagne.layers.Conv2DLayer(
+        ##     lasagne.layers.InputLayer(l_in_to_hid.output_shape),
+        ##     n_out_filters, filter_shape, pad='same')
+        #l_hid_to_hid = DenseLSTMLayer(
+            #lasagne.layers.InputLayer(l_in_to_hid.output_shape),
+            #num_units= 500)
+        ##l_hid_to_hid.input_shape = l_hid_to_hid.input_shapes[0] #ISSUE: SEEMS VERY UNSAFE.
+
+        #l_rec = lasagne.layers.CustomRecurrentLayer(
+             #l_in, l_in_to_hid, l_hid_to_hid)
+
+        #l_reshape = lasagne.layers.ReshapeLayer(l_rec, (-1, np.prod(l_rec.output_shape[2:])))
+
+        #l_out = lasagne.layers.DenseLayer(
+            #l_reshape, num_units= output_size,
+            #nonlinearity=lasagne.nonlinearities.linear)
+
+        #self.layers = [l_in, l_rec, l_out]
+        #self.network = l_out
+
+        ##Convolutions
+        ##prev_layer = lasagne.layers.InputLayer((None, n_channels, width, height))
+        ##for cp in range(num_convpools):
+            ##conv = lasagne.layers.Conv2DLayer(
+                ##incoming = prev_layer,
+                ##num_filters= num_filters,
+                ##filter_size=3, stride= 1, pad=1,
+                ##nonlinearity=lasagne.nonlinearities.tanh,
+                ##W=lasagne.init.GlorotUniform())
+            ##self.layers.append(conv)
+
+            ##pool = lasagne.layers.MaxPool2DLayer(conv, pool_size=4, stride=4)
+            ###self.layers.append(pool)
+
+            ##prev_layer = pool
+
+        ##l_hid = lasagne.layers.DenseLayer(
+            ##pool, num_units= hidden_size,
+            ##nonlinearity=lasagne.nonlinearities.tanh, #Rectify may be causing nans in cross entropy loss.
+            ##W=lasagne.init.GlorotUniform())
+        ###self.layers.append(l_hid3)
+
+        ##l_lstm = lasagne.layers.LSTMLayer(l_hid, num_units= hidden_size)
+        ##l_out = lasagne.layers.DenseLayer(
+            ##l_lstm, num_units= output_size,
+            ##nonlinearity=lasagne.nonlinearities.linear)
+
+        #self.network = l_out
+        #self.initiliaze(mode= 'classify')
+
+        ##Recurrence
+        ##l_in_to_hid = lasagne.layers.Conv2DLayer(
+             ##lasagne.layers.InputLayer((None, n_channels, width, height)),
+             ##n_out_filters, filter_shape, pad='same')
+        ##l_hid_to_hid = lasagne.layers.Conv2DLayer(
+             ##lasagne.layers.InputLayer(l_in_to_hid.output_shape),
+             ##n_out_filters, filter_shape, pad='same')
+        ##l_rec = lasagne.layers.CustomRecurrentLayer(
+             ##l_in, l_in_to_hid, l_hid_to_hid)
+
+        ##l_reshape = lasagne.layers.ReshapeLayer(l_rec, (-1, np.prod(l_rec.output_shape[2:])))
+
+        ##l_out = lasagne.layers.DenseLayer(
+            ##l_reshape, num_units= output_size,
+            ##nonlinearity=lasagne.nonlinearities.linear)
+
+        ##self.layers = [l_in, l_rec, l_out]
+        ##self.network = l_out
+
+        ##self.initiliaze(mode= 'classify')
+
+class DenseLSTMLayer(lasagne.layers.Layer):
+    def __init__(self, incoming, num_units, name=None, **kwargs):
+        #lasagne.layers.Layer.__init__(self)
+        super(DenseLSTMLayer, self).__init__(incoming, **kwargs)
+        (batch_size, in_units) = incoming.output_shape
+
+        dense = lasagne.layers.DenseLayer(
+            lasagne.layers.InputLayer((None, in_units)), num_units= 500,
+            nonlinearity=lasagne.nonlinearities.tanh, #Rectify may be causing nans in cross entropy loss.
+            W=lasagne.init.GlorotUniform())
+
+        lstm = lasagne.layers.LSTMLayer(dense, num_units= 500)
+        self.num_units= 500
+
+        self.only_return_final = lstm.only_return_final
+        self.mask_incoming_index = lstm.mask_incoming_index
+        self.hid_init_incoming_index = lstm.hid_init_incoming_index
+        self.cell_init_incoming_index = lstm.cell_init_incoming_index
+        self.W_in_to_ingate = lstm.W_in_to_ingate
+        self.W_in_to_forgetgate = lstm.W_in_to_forgetgate
+        self.W_in_to_cell = lstm.W_in_to_cell
+        self.W_in_to_outgate = lstm.W_in_to_outgate
+        self.W_hid_to_ingate = lstm.W_hid_to_ingate
+        self.W_hid_to_forgetgate = lstm.W_hid_to_forgetgate
+        self.W_hid_to_cell = lstm.W_hid_to_cell
+        self.W_hid_to_outgate = lstm.W_hid_to_outgate
+        self.b_ingate = lstm.b_ingate
+        self.b_forgetgate = lstm.b_forgetgate
+        self.b_cell = lstm.b_cell
+        self.b_outgate = lstm.b_outgate
+        self.precompute_input = lstm.precompute_input
+
+        self.input_shapes = lstm.input_shapes
+
+    @property
+    def output_shape(self):
+        return self.get_output_shape_for(self.input_shapes)
+
+    def get_output_shape_for(self, input_shapes):
+        # The shape of the input to this layer will be the first element
+        # of input_shapes, whether or not a mask input is being used.
+        input_shape = input_shapes[0]
+        # When only_return_final is true, the second (sequence step) dimension
+        # will be flattened
+        if self.only_return_final:
+            return input_shape[0], self.num_units
+        # Otherwise, the shape will be (n_batch, n_steps, num_units)
+        else:
+            return input_shape[0], input_shape[1], self.num_units
+
+    def get_output_for(self, inputs, **kwargs):
+        # Retrieve the layer input
+        input = inputs[0]
+        # Retrieve the mask when it is supplied
+        mask = None
+        hid_init = None
+        cell_init = None
+        if self.mask_incoming_index > 0:
+            mask = inputs[self.mask_incoming_index]
+        if self.hid_init_incoming_index > 0:
+            hid_init = inputs[self.hid_init_incoming_index]
+        if self.cell_init_incoming_index > 0:
+            cell_init = inputs[self.cell_init_incoming_index]
+
+        # Treat all dimensions after the second as flattened feature dimensions
+        if input.ndim > 3:
+            input = T.flatten(input, 3)
+
+        # Because scan iterates over the first dimension we dimshuffle to
+        # (n_time_steps, n_batch, n_features)
+        input = input.dimshuffle(1, 0, 2)
+        seq_len, num_batch, _ = input.shape
+
+        # Stack input weight matrices into a (num_inputs, 4*num_units)
+        # matrix, which speeds up computation
+        W_in_stacked = T.concatenate(
+            [self.W_in_to_ingate, self.W_in_to_forgetgate,
+             self.W_in_to_cell, self.W_in_to_outgate], axis=1)
+
+        # Same for hidden weight matrices
+        W_hid_stacked = T.concatenate(
+            [self.W_hid_to_ingate, self.W_hid_to_forgetgate,
+             self.W_hid_to_cell, self.W_hid_to_outgate], axis=1)
+
+        # Stack biases into a (4*num_units) vector
+        b_stacked = T.concatenate(
+            [self.b_ingate, self.b_forgetgate,
+             self.b_cell, self.b_outgate], axis=0)
+
+        if self.precompute_input:
+            # Because the input is given for all time steps, we can
+            # precompute_input the inputs dot weight matrices before scanning.
+            # W_in_stacked is (n_features, 4*num_units). input is then
+            # (n_time_steps, n_batch, 4*num_units).
+            input = T.dot(input, W_in_stacked) + b_stacked
+
+        # At each call to scan, input_n will be (n_time_steps, 4*num_units).
+        # We define a slicing function that extract the input to each LSTM gate
+        def slice_w(x, n):
+            return x[:, n*self.num_units:(n+1)*self.num_units]
+
+        # Create single recurrent computation step function
+        # input_n is the n'th vector of the input
+        def step(input_n, cell_previous, hid_previous, *args):
+            if not self.precompute_input:
+                input_n = T.dot(input_n, W_in_stacked) + b_stacked
+
+            # Calculate gates pre-activations and slice
+            gates = input_n + T.dot(hid_previous, W_hid_stacked)
+
+            # Clip gradients
+            if self.grad_clipping:
+                gates = theano.gradient.grad_clip(
+                    gates, -self.grad_clipping, self.grad_clipping)
+
+            # Extract the pre-activation gate values
+            ingate = slice_w(gates, 0)
+            forgetgate = slice_w(gates, 1)
+            cell_input = slice_w(gates, 2)
+            outgate = slice_w(gates, 3)
+
+            if self.peepholes:
+                # Compute peephole connections
+                ingate += cell_previous*self.W_cell_to_ingate
+                forgetgate += cell_previous*self.W_cell_to_forgetgate
+
+            # Apply nonlinearities
+            ingate = self.nonlinearity_ingate(ingate)
+            forgetgate = self.nonlinearity_forgetgate(forgetgate)
+            cell_input = self.nonlinearity_cell(cell_input)
+
+            # Compute new cell value
+            cell = forgetgate*cell_previous + ingate*cell_input
+
+            if self.peepholes:
+                outgate += cell*self.W_cell_to_outgate
+            outgate = self.nonlinearity_outgate(outgate)
+
+            # Compute new hidden unit activation
+            hid = outgate*self.nonlinearity(cell)
+            return [cell, hid]
+
+
+class ConvPoolLayer(lasagne.layers.Layer):
+    def __init__(self, incoming, n_out_filters, filter_shape, pad='same', name=None, **kwargs):
+        #lasagne.layers.Layer.__init__(self)
+        super(ConvPoolLayer, self).__init__(incoming, **kwargs)
+        (batch_size, n_channels, width, height) = incoming.output_shape
+
+        conv = lasagne.layers.Conv2DLayer(
+            lasagne.layers.InputLayer((None, n_channels, width, height)),
+            n_out_filters, filter_shape, pad='same')
+
+        pool = lasagne.layers.MaxPool2DLayer(conv, pool_size=4, stride=4)
+
+        self.num_units = 500
+        dense = lasagne.layers.DenseLayer(
+            pool, num_units= self.num_units,
+            nonlinearity=lasagne.nonlinearities.tanh, #Rectify may be causing nans in cross entropy loss.
+            W=lasagne.init.GlorotUniform())
+
+        self.W = dense.W
+        self.b = dense.b
+        self.nonlinearity = dense.nonlinearity
+
+
+    @property
+    def output_shape(self):
+        return self.get_output_shape_for(self.input_shape)
+
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0], self.num_units)
+
+    def get_output_for(self, input, **kwargs):
+        if input.ndim > 2:
+            # if the input has more than two dimensions, flatten it into a
+            # batch of feature vectors.
+            input = input.flatten(2)
+
+        activation = T.dot(input, self.W)
+        if self.b is not None:
+            activation = activation + self.b.dimshuffle('x', 0)
+        return self.nonlinearity(activation)
+
+
+
+
+
+
